de7ce55019e71eec35ee8abe48d448d1a824fa02
Himanshu Jain <himanshujain501@gmail.com>
2018-12-31 16:19:37 +0530

    some more file added

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Model\student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function show(student $student)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Model\student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(student $student)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Model\student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, student $student)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Model\student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(student $student)
+    {
+        //
+    }
+}
---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Model\student::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
